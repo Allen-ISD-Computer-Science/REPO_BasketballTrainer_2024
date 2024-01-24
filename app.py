95b72bd0c287035a9efaf59c6ad5d941d1876f1f
AravLohe <arav.lohe@student.allenisd.org>
2024-01-24 15:13:08 -0600

    Added button for Home UI and other widgets. Still have to do title.

--- app.py
+++ app.py
@@ -1,13 +1,61 @@
 import ultralytics
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
-import cv2
-
-app = QApplication([])
-window = QWidget()
-window.setWindowTitle("PyQt App")
-window.setGeometry(100, 100, 280, 80)
-helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
-helloMsg.move(60, 15)
-window.show()
-sys.exit(app.exec())
+from PyQt6.QtWidgets import * 
+from PyQt6.QtGui import * 
+from PyQt6.QtCore import * 
+import sys 
+  
+  
+class Window(QMainWindow): 
+    def __init__(self): 
+        super().__init__() 
+  
+        # setting title 
+        self.setWindowTitle("AI Basketball Trainer ") 
+  
+        # setting geometry 
+        self.setGeometry(100, 100, 600, 400) 
+  
+        # calling method 
+        self.UiComponents() 
+  
+        # showing all the widgets 
+        self.show() 
+  
+    # method for widgets 
+    def UiComponents(self): 
+  
+        # creating a push button 
+        FilmButton = QPushButton("Film Video ", self) 
+  
+        # setting geometry of button 
+        FilmButton.setGeometry(250, 150, 100, 40) 
+  
+  
+        # adding action to a button 
+        FilmButton.clicked.connect(self.clickme) 
+        # creating a push button 
+        UploadButton = QPushButton("Video Upload ", self) 
+  
+        # setting geometry of button 
+        UploadButton.setGeometry(250, 250, 100, 40) 
+  
+  
+        # adding action to a button 
+        UploadButton.clicked.connect(self.clickme) 
+  
+  
+    # action method 
+    def clickme(self): 
+  
+        # printing pressed 
+        print("Working") 
+  
+# create pyqt5 app 
+App = QApplication(sys.argv) 
+  
+# create the instance of our Window 
+window = Window() 
+  
+# start the app 
+sys.exit(App.exec()) 

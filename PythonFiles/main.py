8004546c8ecf6b3fb4151ef8def6cc9c510bc0bb
onikh <onikh738@gmail.com>
2024-02-03 19:58:29 -0600

    cv2 implementation of model

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -1,14 +1,34 @@
 from ultralytics import YOLO
-
-from collections import defaultdict
-
 import cv2
 import threading
 
 poseModel = YOLO("yolov8n-pose.pt")
 basketballModel = YOLO(r"C:\Users\onikh\Desktop\Projects\REPO_BasketballTrainer_2024\runs\detect\train6\weights\best.pt")
 
-results = basketballModel.predict(source="0", show=True)
+capture = cv2.VideoCapture(0)
+
+if not capture.isOpened():
+    print("No stream")
+    exit()
+
+while(True):
+    ret, frame = capture.read()
+
+    results = basketballModel.predict(frame, show=True)
+
+    for result in results:
+        print(result.boxes.xywh)
+
+    if not ret:
+        print("No more stream")
+        break
+
+    cv2.imshow("Webcam!", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows
 
 
-print(results)

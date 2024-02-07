e6fb6fb859edd9dc0cac8038c89f928bc041c11f
onikh <onikh738@gmail.com>
2024-02-06 20:11:32 -0600

    made class for Frame and made test file to track ball that prints Xcoordinates of the first ball in the objects detected array

---
+++ testResults.py
@@ -0,0 +1,74 @@
+
+from collections import defaultdict
+from ultralytics import YOLO
+import cv2
+import threading
+
+
+
+
+
+
+
+
+
+
+basketballModel = YOLO(r"C:\Users\onikh\Desktop\Projects\REPO_BasketballTrainer_2024\runs\detect\train6\weights\best.pt")
+
+
+
+#create a cv2 object that takes a video frame by frame
+capture = cv2.VideoCapture(0)
+fps = capture.get(cv2.CAP_PROP_FPS)
+print(fps)
+
+
+if not capture.isOpened():
+    print("No stream")
+    exit()
+
+while(True):
+    ret, frame = capture.read()
+
+    
+
+    basketballResults = basketballModel.track(frame, show=False, persist=True, tracker="bytetrack.yaml", verbose=False, conf= 0.5)
+    labeledFrame = basketballResults[0].plot()
+    cv2.imshow("tracker", labeledFrame)
+
+    for result in basketballResults:
+        boxes = result.boxes.cpu().numpy()
+        
+
+        xyxys = boxes.xyxy
+
+        if xyxys.size > 0: #check if xyxys is empty
+            if xyxys[0] is not None:
+                print("X Coordinate: " + str(xyxys[0][0]))
+        else:
+            print("xyxys is empty")
+
+        
+
+
+
+
+        
+
+
+    if not ret:
+        print("No more stream")
+        break
+
+    
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows
+
+
+
+
+

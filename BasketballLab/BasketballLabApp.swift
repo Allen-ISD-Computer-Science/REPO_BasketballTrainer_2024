128e34b5c42f4c74a33064cdc70012b436cf714d
onikh <onikh738@gmail.com>
2024-02-18 15:41:03 -0600

    initial commit

---
+++ BasketballLab/BasketballLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballLabApp.swift
+//  BasketballLab
+//
+//  Created by Onik Hoque on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BasketballLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasketballLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasketballLab
+//
+//  Created by Onik Hoque on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
